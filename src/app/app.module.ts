a99c5ac6fe9c538dd156125dff4f52cc4574213a
Pierre-Louis Franc <pierre-louis.franc@epfedu.fr>
2019-09-10 15:37:22 +0200

    ajout du composant photo de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfilPictureComponent } from './profil-picture/profil-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilPictureComponent
   ],
   imports: [
     BrowserModule
