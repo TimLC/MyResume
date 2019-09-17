9433a5de12f0a43370658280dba0201b3b1c295f
Pierre-Louis Franc <pierre-louis.franc@epfedu.fr>
2019-09-17 16:50:09 +0200

    ajout des models avec const get set

--- src/Model/skill.ts
+++ src/Model/skill.ts
@@ -1,11 +1,28 @@
 import { defaultsDeep } from 'lodash';
 
 export class skill {
-  id: number;
-  skillName: string;
+  private _id: number;
+  private _skillName: string;
 
 
-  constructor(skill?: Partial<skill>) {
-    defaultsDeep(this, skill);
+  constructor(id: number, skillName: string) {
+    this._id = id;
+    this._skillName = skillName;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get skillName(): string {
+    return this._skillName;
+  }
+
+  set skillName(value: string) {
+    this._skillName = value;
   }
 }
