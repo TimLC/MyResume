b7f1a965841ea1322f6f1969f5a199b05dacf5d9
Pol frx <pol.2frx@hotmail.fr>
2019-09-21 18:22:04 +0200

    change private attributes to public

--- src/Model/skill.ts
+++ src/Model/skill.ts
@@ -1,28 +1,11 @@
-import { defaultsDeep } from 'lodash';
 
-export class skill {
-  private _id: number;
-  private _skillName: string;
+export class Skill {
+  id: number;
+  skillName: string;
 
 
   constructor(id: number, skillName: string) {
-    this._id = id;
-    this._skillName = skillName;
-  }
-
-  get id(): number {
-    return this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get skillName(): string {
-    return this._skillName;
-  }
-
-  set skillName(value: string) {
-    this._skillName = value;
+    this.id = id;
+    this.skillName = skillName;
   }
 }
